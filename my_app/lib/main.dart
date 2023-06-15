db2e2cc37ca0fcf3973227dbab1ae0ffc60ecb1f
aksh-nishar <akshnishar18@gmail.com>
2023-06-15 12:46:29 +0530

    :sparkles: Add pages folder and new features
    Added login_page and home_page in pages folder.
    
    Introduced routes, themes (light/dark), and also designed a basic login
    page.

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/home_page.dart';
+import 'package:my_app/pages/home_page.dart';
+import 'package:my_app/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primaryColor: Colors.deepPurple,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/" : (context) => LoginPage(),
+        "/home" : (context) => HomePage(),
+        "/login" : (context) => LoginPage(),
+      },
     );
   }
 }
+  
--- my_app/lib/pages/home_page.dart
+++ my_app/lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     const int days = 30;
@@ -8,7 +10,7 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Welcome Catalog"),
+        title: Text("Catalog App"),
       ),
       body: Center(
         child: Container(
