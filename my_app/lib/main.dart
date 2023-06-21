f13047fe3abfb1fdd1bc5f6d0025c3e4361d8ca7
aksh-nishar <akshnishar18@gmail.com>
2023-06-21 14:27:21 +0530

    :lipstick: Add a ListView builder for items
    Created an item widget, and a ListView to display the item widget 20
    times in a scrollable view.

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => const LoginPage(),
-        MyRoutes.homeRoute: (context) => const HomePage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
         MyRoutes.loginRoute: (context) => const LoginPage(),
       },
     );
--- my_app/lib/pages/home_page.dart
+++ my_app/lib/pages/home_page.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/models/catalog.dart';
 import 'package:my_app/widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+
+  HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const int days = 30;
-    const String name = "Aksh";
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
         centerTitle: true,
       ),
-      body: Center(
-        child: Container(
-          child: const Text("Welcome to $days days of flutter by $name!"),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: const MyDrawer(),
