84490da9ea976c8a798271a1e90c91a9ede62729
aksh-nishar <akshnishar18@gmail.com>
2023-06-21 15:56:47 +0530

    :monocle_face: Load and decode JSON data

--- my_app/lib/pages/home_page.dart
+++ my_app/lib/pages/home_page.dart
@@ -1,15 +1,35 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:my_app/models/catalog.dart';
 import 'package:my_app/widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
-  final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
-
+class HomePage extends StatefulWidget {
   HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJSON =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJSON);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
