7b123cab3785ac6a2836a1b7e8e5739cea91f79a
aksh-nishar <akshnishar18@gmail.com>
2023-06-25 23:07:55 +0530

    :lipstick: Update cart page
    Display list of dummy items selected and also a dummy total and buy
    button.

--- my_app/lib/pages/cart_page.dart
+++ my_app/lib/pages/cart_page.dart
@@ -13,6 +13,66 @@ class CartPage extends StatelessWidget {
         centerTitle: true,
         title: "Cart".text.color(context.theme.hintColor).make(),
       ),
+      body: Column(
+        children: [
+          const _CartList().p32().expand(),
+          const Divider(),
+          const _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$9999".text.xl4.color(context.theme.hintColor).make(),
+          30.widthBox,
+          SizedBox(
+            width: 100,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(context.theme.highlightColor),
+              ),
+              child: "Buy".text.color(Colors.white).make(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList();
+
+  @override
+  State<_CartList> createState() => __CartListState();
+}
+
+class __CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: const Icon(Icons.done),
+        title: "Item 1".text.make(),
+        trailing: IconButton(
+          icon: const Icon(Icons.remove_circle_outline),
+          onPressed: () {},
+        ),
+      ),
     );
   }
 }
