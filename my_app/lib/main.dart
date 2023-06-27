6c484f697f2d69b5c008926d53423273a7cba4df
aksh-nishar <akshnishar18@gmail.com>
2023-06-27 14:57:37 +0530

    :recycle: Create VxStore, convert stateful widgets to stateless widgets
    Create VxStore to store all the app data in a single store.
    
    Convert class AddToCart and _CartList to stateless widgets.

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/cart/store.dart';
 import 'package:my_app/pages/cart_page.dart';
 import 'package:my_app/pages/home_page.dart';
 import 'package:my_app/pages/login_page.dart';
 import 'package:my_app/utils/routes.dart';
 import 'package:my_app/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(VxState(store: MyStore(), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
--- my_app/lib/widgets/home_widgets/add_to_cart.dart
+++ my_app/lib/widgets/home_widgets/add_to_cart.dart
@@ -4,20 +4,15 @@ import 'package:velocity_x/velocity_x.dart';
 import '../../models/cart.dart';
 import '../../models/catalog.dart';
 
-class AddToCart extends StatefulWidget {
+class AddToCart extends StatelessWidget {
   final Item catalog;
-  const AddToCart(this.catalog, {super.key});
+  AddToCart(this.catalog, {super.key});
 
-  @override
-  State<AddToCart> createState() => _AddToCartState();
-}
-
-class _AddToCartState extends State<AddToCart> {
   final _cart = CartModel();
 
   @override
   Widget build(BuildContext context) {
-    bool isInCart = _cart.items.contains(widget.catalog);
+    bool isInCart = _cart.items.contains(catalog);
 
     return ElevatedButton(
       onPressed: () {
@@ -25,8 +20,8 @@ class _AddToCartState extends State<AddToCart> {
           isInCart = isInCart.toggle();
           final _catalog = CatalogModel();
           _cart.catalog = _catalog;
-          _cart.add(widget.catalog);
-          setState(() {});
+          _cart.add(catalog);
+          // setState(() {});
         }
       },
       style: ButtonStyle(
